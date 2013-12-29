b4a9f56b5fd42bd90677726f8910d68d0f660b81
hangstrap <hangstrap@gmail.com>
2013-12-29 23:32:51 +1300

    Random_photo_list looking good

--- test/random_photo_list_test.dart
+++ test/random_photo_list_test.dart
@@ -1,54 +1,116 @@
 
 import 'package:unittest/unittest.dart';
 import '../web/random_photo_list.dart' ;
+import "dart:math";
+import 'package:unittest/mock.dart';
 
 void main(){
 
+  MockRandom random;
   RandomPhotoList<int> underTest;
   
-  List<int> firstItems = createItems( 0, 9);  
+  List<int> firstItems = createItems( 0, 9);
+  List<int> secondItems = createItems( 10, 19);    
+  
   setUp((){
-    print( "setup");
     underTest = new RandomPhotoList<int>();
+    RandomPhotoList.random = random = new MockRandom();
 
   });
 
-  group("Basic", (){
-    test("should be able to add list",() {
+  group("Adding lists of items", (){
+    test("should be added to items list",() {
       
       underTest.addList( firstItems );
-      expect( underTest.items, unorderedEquals( firstItems ));
+      expect( underTest.items, orderedEquals( firstItems ));
+    });
+    test("should be added to originalItems list",() {
+      
+      underTest.addList( firstItems );
+      expect( underTest.originalItems, orderedEquals( firstItems ));
     });
     test("should be able to add multiple lists",() {
       
       underTest.addList( firstItems);
-      
-      List<int> secondItems = createItems( 10, 19);
       underTest.addList( secondItems);
       
       var total = [];
       total.addAll( firstItems);
-      total.addAll( secondItems);
-      
-      expect( underTest.items, unorderedEquals( total));
+      total.addAll( secondItems);      
+      expect( underTest.items, orderedEquals( total));
     });  
+  });    
+  group( "sub list", (){
     
-    group( "sub list", (){
-      test("when have less items than requested then should return all items",(){
+    group( "when have less items than requested", (){
+      test("then should return all items",(){
   
         underTest.addList( firstItems );
         List<int> subItems = underTest.randomSubList( length:10);      
         expect( subItems, unorderedEquals( firstItems ));
         
       });
-      test("when have more items than requested then should return list with length matching requested",(){
+      test("then items list should be repoputated with orignal list",(){
         
         underTest.addList( firstItems );
+        List<int> subItems = underTest.randomSubList( length:10);      
+        expect( underTest.items, orderedEquals( underTest.originalItems ));
+        
+      });
+
+    });      
+    group( "when more items than requested", (){
+      
+      setUp((){
+        random.when( callsTo("nextInt", anything)).thenReturn( 2);               
+        underTest.addList( firstItems );          
+      });
+    
+      test("then should return list with length matching requested",(){
+        
         List<int> subItems = underTest.randomSubList( length:5);      
         expect( subItems, hasLength( 5));        
       });
+      test("should have list starting from value returned by random",(){
+        
+        List<int> subItems = underTest.randomSubList( length:5);
+        expect( subItems, orderedEquals( [2,3,4,5,6]));
+      });
+      test("subitems should be removed from the original list", (){
+        List<int> subItems = underTest.randomSubList( length:5);
+        expect( underTest.items, orderedEquals( [0,1,7,8,9]));
+      });
+      test("when items are low then original items should be added to list", (){
+        underTest.randomSubList( length:5);
+        underTest.randomSubList( length:5);
+        expect( underTest.items, orderedEquals( underTest.originalItems));
+      });
+    });    
+    group("nextItem", (){
+      
+      test("Should return correct patten", (){
+
+        underTest.addList( firstItems );
+        random.when( callsTo("nextInt", anything)).thenReturn( 2).thenReturn( 0, 4);        
+        expect( underTest.nextItem( length:3), equals(2));
+        expect( underTest.nextItem( length:3), equals(3));
+        expect( underTest.nextItem( length:3), equals(4));        
+        //random will be called, which will return 0
+        expect( underTest.nextItem( length:3), equals(0));
+        expect( underTest.nextItem( length:3), equals(1));
+        expect( underTest.nextItem( length:3), equals(5));
+        //random will be called, which will return 0
+        expect( underTest.nextItem( length:3), equals(6));
+        expect( underTest.nextItem( length:3), equals(7));
+        expect( underTest.nextItem( length:3), equals(8));
+        //random will be called, which will return 0        
+        //This should reload the list
+        expect( underTest.nextItem( length:3), equals(0));
+        expect( underTest.nextItem( length:3), equals(1));
+        expect( underTest.nextItem( length:3), equals(2));
+        
+      });
     });
-    
   });
 }
 
@@ -58,4 +120,8 @@ List<int> createItems(int from, int to) {
     list.add( i);
   }
   return list;
+}
+@proxy
+class MockRandom extends Mock implements Random{
+  
 }
--- web/random_photo_list.dart
+++ web/random_photo_list.dart
@@ -3,24 +3,44 @@ import "dart:math";
 class RandomPhotoList<T>{
   
   static Random random = new Random();
-  
+  List<T> originalItems =[];
   List<T> items = [];
+  List<T> nextItemList = [];
   
   void addList(List<T> items) {
     this.items.addAll( items);
+    this.originalItems.addAll( items);
   }
   
-  
-  List<int> randomSubList({int length: 10}) {
-    if( length >= items.length){
-      return items;      
-    }else{
-      var result = [];
-      for( int i=0; i < length; i++){
-        result.add( items.getRange(getRandom(length), length));
-      }
-      return result;
+  T nextItem( {int length: 10}){
+    if( nextItemList.isEmpty){
+      nextItemList = randomSubList(length: length);
     }
+    return nextItemList.removeAt( 0);
+  }
+  
+  List<T> randomSubList({int length: 10}) {
+    
+    if( length >= items.length){
+      return _listIsAlmostEmpty();      
+    }else{
+      return _listIsFullEnough(length);
+    }
+  }
+
+  List<T> _listIsFullEnough(int length) {
+    int startFrom = getRandom(length);
+    int endsAt = startFrom + length;        
+    List<T> result = new List<T>.from( items.getRange( startFrom, endsAt));
+    items.removeRange( startFrom, endsAt);
+    return result;
+  }
+
+  List<T> _listIsAlmostEmpty() {
+    List<T> result = items;
+    items.clear();
+    items.addAll( originalItems);
+    return result;
   }
   
   int getRandom( int length){
