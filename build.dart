7949a519d0c70eb252376975fe8f48633790e6fc
hangstrap <hangstrap@gmail.com>
2014-02-11 21:15:23 +1300

    Downloading image of the correct resolution

--- build.dart
+++ build.dart
@@ -1,7 +1,8 @@
 import 'package:polymer/builder.dart';
         
 main(args) { 
-
+  lint(entryPoints: ['web/picasawebalbumdisplay.html'], options: parseOptions(args));
+  
   build(entryPoints: ['web/picasawebalbumdisplay.html'],
         options: parseOptions(args));
 }
--- web/picasaphoto.dart
+++ web/picasaphoto.dart
@@ -2,6 +2,7 @@ import 'package:polymer/polymer.dart';
 import 'picasa_web_albums.dart';
 import 'dart:html';
 import "dart:async";
+import "dart:math";
 import "random_photo_list.dart";
 
 /**
@@ -16,22 +17,30 @@ class PicasaPhoto extends PolymerElement {
   @observable
   String imageUrl = "https://lh5.googleusercontent.com/-IvEtX1Hcztg/UoaKrLqG3-I/AAAAAAAAT7E/9jyfqPDIl5c/s1200/IMG_5271.JPG";
   @observable  
-  String title = "";
+  String title = "cute puppy";
   @observable
   String delay = '3';
+  
+  Element imgElement;
 
   StreamSubscription nextPhotoSubscription;
     
   PicasaPhoto.created() : super.created() {
     
+    imgElement = shadowRoot.querySelector('#img-tag');
     User user = new User( "101488109748928583216", getHttpRequest);
     user.albums().then( _processAlbums);
   }
 
   void delayChanged( ){
     print( "had event ${delay}");
+    
+
+    print( "width=${imgElement.clientWidth} height=${imgElement.clientHeight}");
     _setUpStream();
   }
+  
+
   
   _processAlbums(List<Album> albums) {
     albums.forEach( (album)=>album.photos.then( _processPhotos));
@@ -47,12 +56,17 @@ class PicasaPhoto extends PolymerElement {
   void _displayNextPhoto(){
     current = randomPhotoList.nextItem(); 
     if( current != null){
-      imageUrl = current.url(imgmax: 600);
-    }else{
+      imageUrl = current.url(imgmax: _getImgageMaxSize());
+      title= "Album '${current.album.title}'  ${current.summary}";
+          }else{
       print( "nothing to display");
     }    
-
   }
+  
+  int _getImgageMaxSize(){
+    return max( imgElement.clientHeight, imgElement.clientWidth);
+  }
+
   
   void _setUpStream(){
     
